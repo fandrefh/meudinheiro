a391f4deac547eff0dc804905ce064f689a5d0d4
Francisco André <fandrefh@gmail.com>
2022-05-27 21:26:37 -0300

    Tela de categorias

--- apps/financas/urls.py
+++ apps/financas/urls.py
@@ -5,5 +5,7 @@ app_name = 'financas'
 from . import views
 
 urlpatterns = [
+    path('nova-categoria/', views.nova_categoria, name='nova_categoria'),
+    path('lista-categorias/', views.lista_categorias, name='lista_categorias'),
     path('', views.principal, name='principal'),
 ]
--- apps/financas/views.py
+++ apps/financas/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+
+from .models import Categoria
+from .forms import CategoriaForm
 
 # Create your views here.
 
@@ -8,4 +12,30 @@ from django.contrib.auth.decorators import login_required
 def principal(request):
     template_name = 'financas/principal.html'
     context = {}
+    return render(request, template_name, context)
+
+
+def nova_categoria(request):
+    template_name = 'financas/nova_categoria.html'
+    context = {}
+    if request.method == 'POST':
+        form = CategoriaForm(request.POST)
+        if form.is_valid():
+            cat_form = form.save(commit=False)
+            cat_form.usuario = request.user
+            cat_form.save()
+            messages.success(request, 'Categoria adicionada com sucesso.')
+            return redirect('financas:lista_categorias')
+    else:
+        form = CategoriaForm()
+    context['form'] = form
+    return render(request, template_name, context)
+
+
+def lista_categorias(request):
+    template_name = 'financas/lista_categorias.html'
+    categorias = Categoria.objects.filter(usuario=request.user) # ORM -> SELECT * FROM categorias WHERE usuario = username
+    context = {
+        'categorias': categorias
+    }
     return render(request, template_name, context)
