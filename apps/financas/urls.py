59a6eebe8df91247aae49d49d261a6f5e2617afc
Francisco André <fandrefh@gmail.com>
2022-05-30 21:24:08 -0300

    Mais recursos

--- apps/financas/urls.py
+++ apps/financas/urls.py
@@ -7,5 +7,9 @@ from . import views
 urlpatterns = [
     path('nova-categoria/', views.nova_categoria, name='nova_categoria'),
     path('lista-categorias/', views.lista_categorias, name='lista_categorias'),
+    path('editar-categoria/<int:pk>/', views.editar_categoria, name='editar_categoria'),
+    path('apagar-categoria/<int:pk>/', views.apagar_categoria, name='apagar_categoria'),
+    path('nova-receita/', views.nova_receita, name='nova_receita'),
+    path('lista-receitas/', views.lista_receitas, name='lista_receitas'),
     path('', views.principal, name='principal'),
 ]
--- apps/financas/views.py
+++ apps/financas/views.py
@@ -1,9 +1,11 @@
 from django.contrib import messages
-from django.shortcuts import render, redirect
+from django.http import Http404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
-from .models import Categoria
-from .forms import CategoriaForm
+from .models import Categoria, Receita
+from .forms import CategoriaForm, ReceitaForm
+
 
 # Create your views here.
 
@@ -37,5 +39,64 @@ def lista_categorias(request):
     categorias = Categoria.objects.filter(usuario=request.user) # ORM -> SELECT * FROM categorias WHERE usuario = username
     context = {
         'categorias': categorias
+    }
+    return render(request, template_name, context)
+
+
+def editar_categoria(request, pk):
+    template_name = 'financas/nova_categoria.html'
+    context = {}
+    # categoria = get_object_or_404(Categoria, id=pk) # ORM -> SELECT * FROM categoria WHERE id = pk
+    # categoria = Categoria.objects.filter(pk=pk, usuario=request.user).first() # ORM -> SELECT * FROM categoria WHERE id = pk
+    try:
+        categoria = Categoria.objects.get(pk=pk, usuario=request.user) # ORM -> SELECT * FROM categoria WHERE id = pk
+    except Categoria.DoesNotExist as e:
+        messages.warning(request, 'Você não tem permissão para editar a categoria informada.')
+        return redirect('financas:lista_categorias')
+    if request.method == 'POST':
+        form = CategoriaForm(request.POST, instance=categoria)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Categoria atualizada com sucesso.')
+            return redirect('financas:lista_categorias')
+    else:
+        form = CategoriaForm(instance=categoria)
+    context['form'] = form
+    return render(request, template_name, context)
+
+
+def apagar_categoria(request, pk):
+    try:
+        categoria = Categoria.objects.get(pk=pk, usuario=request.user) # ORM -> SELECT * FROM categoria WHERE id = pk
+        categoria.delete()
+    except Categoria.DoesNotExist as e:
+        messages.warning(request, 'Você não tem permissão para apagar a categoria informada.')
+        return redirect('financas:lista_categorias')
+    messages.info(request, 'Categoria apagada.')
+    return redirect('financas:lista_categorias')
+
+
+def nova_receita(request):
+    template_name = 'financas/nova_receita.html'
+    context = {}
+    if request.method == 'POST':
+        form = ReceitaForm(request.POST)
+        if form.is_valid():
+            receita_form = form.save(commit=False)
+            receita_form.usuario = request.user
+            receita_form.save()
+            messages.success(request, 'Receita adicionada com sucesso')
+            return redirect('financas:lista_receitas')
+    else:
+        form = ReceitaForm()
+    context['form'] = form
+    return render(request, template_name, context)
+
+
+def lista_receitas(request):
+    template_name = 'financas/lista_receitas.html'
+    receitas = Receita.objects.filter(usuario=request.user)
+    context = {
+        'receitas': receitas
     }
     return render(request, template_name, context)
